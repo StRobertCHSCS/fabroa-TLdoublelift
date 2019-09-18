90f0d2624144db3fede71bc6bce66994301a40f3
TLdoublelift <andrew.lui22@ycdsbk12.ca>
2019-09-18 08:54:11 -0400

    print examples

---
+++ Working/ClassExamples/1_1.py
@@ -0,0 +1,10 @@
+print("This is a print statement")
+print("What's wrong with this statement?")
+print("How about this one")
+print(5)
+print(3.14)
+print("Here are the ages of my three children: ", 12, 7, 4)
+print(3.14, "is my favourite pi")
+greeting = "Hello world"
+print(greeting)
+q
